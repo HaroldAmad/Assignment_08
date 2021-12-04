99675b2ca1c1215ba6c44b857ad70d17bdce0906
HaroldAmad <harold.amad@gmail.com>
2021-12-04 14:16:57 +0800

    Final commit)
    git commit -m Final Commit

--- Program02.py
+++ Program02.py
@@ -7,3 +7,10 @@ num = random.randint(0,100)
 
 while game:
     guess = int(input("Your guess: "))
+    if guess > num:
+        print("Greater Than")
+    elif guess < num:
+        print("Less Than")
+    elif guess == num:
+        print("====You are Correct!!!====")
+        break
