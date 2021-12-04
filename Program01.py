2a3c5174aaa299e60d24345b04e5ed67cd81792e
HaroldAmad <harold.amad@gmail.com>
2021-12-04 12:00:30 +0800

    User Input Function

--- Program01.py
+++ Program01.py
@@ -12,3 +12,19 @@ def lottery ():
         winningNumbers.append(number)
     winningNumbers.sort
     return winningNumbers
+
+def guess ():
+    userGuess = []
+
+    for i in range (0,3):
+        hula = int(input("Enter Your number between 0 and 9: "))
+        while (hula in userGuess or hula<0 or hula>9):
+                print("Invalid guess, try again")
+                hula = int(input("Enter Your number between 0 and 9: "))
+    
+        userGuess.append(hula)
+    userGuess.sort
+    return userGuess
+
+winningFunc = lottery()
+userFunc = guess()
